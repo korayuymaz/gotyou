a1997869c7e6ba56147ec06df7f43fff5c117089
Koray <koray.uymaz@agesa.com.tr>
2024-09-22 20:19:45 +0300

    add flex wrap and logo deleted if device width 1028

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,36 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useState, useEffect } from "react";
 import Button from "./ui/Button";
 
 const Navbar = () => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    // Function to update the width
+    const updateWidth = () => {
+      setWidth(window.innerWidth);
+    };
+
+    // Set initial width
+    updateWidth();
+
+    // Add event listener to update width on resize
+    window.addEventListener("resize", updateWidth);
+    // Cleanup function to remove event listener on component unmount
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
   return (
-    <div className="flex justify-between w-full gap-[497.5px]">
-      <div>
-        <Image
-          src={"/logo.svg"}
-          alt="logo"
-          width={0}
-          height={0}
-          style={{ width: "auto", height: "auto" }}
-        />
-      </div>
-      <div className="flex gap-12 text-center text-purple text-[16px]">
+    <div className="flex justify-between w-full xl:gap-[497.5px] lg:gap-2 flex-wrap">
+      {width >= 1028 && (
+        <div>
+          <Image src={"/logo.svg"} alt="logo" width={108} height={63} />
+        </div>
+      )}
+      <div className="flex flex-wrap gap-12 text-center text-purple text-[16px]">
         <Link href="/" className="m-auto">
           HOW DOES IT WORK
         </Link>
